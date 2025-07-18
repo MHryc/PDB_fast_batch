e4af1a314b738f6e510d0adf57a311c848b909dc
mmh <mhry@igr.poznan.pl>
2025-07-18 15:09:49 +0200

    initial commit witha a lot of stuff

---
+++ PDB_fast_batch.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Script to download files from RCSB http file download services.
+# Use the -h switch to get help on usage.
+
+if ! command -v wget &> /dev/null; then
+	echo "'wget' could not be found. You need to install 'wget' for this script to work."
+	exit 1
+fi
+
+PROGNAME=$0
+BASE_URL="https://files.rcsb.org/download/"
+
+usage() {
+	cat << EOF >&2
+Usage: $PROGNAME -f <file> [-o <dir>] [-c] [-p]
+
+ -f <file>: the input file containing one PDB ID per line
+ -o  <dir>: the output dir, default: current dir
+ -c       : download a cif.gz file for each PDB id
+ -p       : download a pdb.gz file for each PDB id (not available for large structures)
+ -a       : download a pdb1.gz file (1st bioassembly) for each PDB id (not available for large structures)
+ -A       : download an assembly1.cif.gz file (1st bioassembly) for each PDB id
+ -x       : download a xml.gz file for each PDB id
+ -s       : download a sf.cif.gz file for each PDB id (diffraction only)
+ -m       : download a mr.gz file for each PDB id (NMR only)
+ -r       : download a mr.str.gz for each PDB id (NMR only)
+EOF
+	exit 1
+}
+
+listfile=""
+outdir="."
+cif=false
+pdb=false
+pdb1=false
+cifassembly1=false
+xml=false
+sf=false
+mr=false
+mrstr=false
+while getopts f:o:cpaAxsmr o; do
+	case $o in
+		(f) listfile=$OPTARG;;
+		(o) outdir=$OPTARG;;
+		(c) cif=true;;
+		(p) pdb=true;;
+		(a) pdb1=true;;
+		(A) cifassembly1=true;;
+		(x) xml=true;;
+		(s) sf=true;;
+		(m) mr=true;;
+		(r) mrstr=true;;
+		(*) usage
+	esac
+done
+shift "$((OPTIND - 1))"
+
+if [ "$listfile" == "" ]; then
+	echo "Parameter -f must be provided"
+	exit 1
+fi
+
+# create output directory if it doesn't exists
+[[ -d $outdir ]] || mkdir -p $outdir
+
+# used to generate temporary files for pasting final URLs
+length=$(wc -l $listfile | cut -d ' ' -f 1)
+
+# make list of URLs for wget -i
+make_list() {
+	size=$1
+	base_url=$2
+	extension=$3
+
+	for i in $(seq $size); do
+		echo $base_url >> base.tmp
+		echo $extension >> extensions.tmp
+	done
+
+	paste -d '' base.tmp $listfile extensions.tmp
+
+	# delete temporary files
+	rm base.tmp extensions.tmp
+}
+
+formats=($cif $pdb $pdb1 $cifassembly1 $xml $sf $mr $mrstr)
+extensions=(
+	".cif.gz" ".pdb.gz" ".pdb1.gz" "-assembly1.cif.gz" 
+	".xml.gz" "-sf.cif.gz" ".mr.gz" "_mr.str.gz"
+)
+
+# loop over file formats and downloads ones picked by the user
+for idx in {0..7}; do
+	if [[ ${formats[$idx]} == true ]]; then
+		extension="${extensions[$idx]}"
+		echo "Downloading *$extension to $outdir"
+		wget -nv -i <(make_list $length $BASE_URL $extension)
+		mv *$extension $outdir
+	fi
+done
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # PDB_fast_batch
-PDB batch download, faster than https://www.rcsb.org/docs/programmatic-access/batch-downloads-with-shell-script
+
+PDB batch download, faster than [one shared by RCSB PDB](https://www.rcsb.org/docs/programmatic-access/batch-downloads-with-shell-script)
+
+1. Instead of downloading each file individually it first creates a list of
+   URLs and passes it to `wget -i`, making it much faster.
+2. Instead of passing file with PDB IDs separated by comas you pass a file with
+   one PDB ID per line, making it easier to create input data from tabular (eg.
+   csv and tsv) files.
+
+## Speed comparison
+
+Both scripts were tested on a list of 54 PDB IDs
+
+Scripts' download speed was measured for cif.gz, pdb.gz, pdb1.gz,
+assembly1.cif.gz and xml.gz for a list of 54 PDB IDs (270 files in total) using
+linux `time`.
+
+In this test `PDB_fast_batch.sh` was around 50 times faster.
+
+### RCSB PDB original
+
+```
+$ time ./batch_download.sh -f abc5_pdbid.csv -cpaAx
+```
+
+```
+real    6m59.701s
+user    0m3.143s
+sys     0m2.035s
+```
+
+### wget based fork
+
+```
+$ time ./PDB_fast_batch.sh -f abc5_pdbid.txt -cpaAx -o test_out
+```
+
+```
+real    0m7.495s
+user    0m0.680s
+sys     0m0.585s
+```
